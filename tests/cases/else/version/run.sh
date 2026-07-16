bc1423ad97054514c117ab7ee37a7057eb39ce51
OGAWA Keiji <70509917+kibi2@users.noreply.github.com>
2026-07-16 17:24:23 +0900

    test: update ci test case else.version

--- tests/cases/else/version/run.sh
+++ tests/cases/else/version/run.sh
@@ -5,4 +5,9 @@ exec > out-actual.txt 2>&1
 . $ROOT_DIR/tests/common.sh
 
 CASE 7 version
-    tir_parser version
+        if tir_parser version | grep -Eq '^[0-9]+\.[0-9]+([.-].*)?$'; then
+            echo OK
+        else
+            echo NG
+            tir_parser version
+        fi
